e8c82a0827ddbcb9e7e2f349e6f2dfcde1911195
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-28 10:11:00 +0700

    SiGG-Payment-Gateway-Series-Transactions

---
+++ SiGG-Payment-Gateway/SiGG-Payment-Gateway/Controllers/TransactionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SiGG_Payment_Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<TransactionsController> _logger;
+
+        public TransactionsController(ILogger<TransactionsController> logger)
+        {
+            _logger = logger;
+        }
+    
+        [HttpPost(Name = "PostTransactions")]
+        public IEnumerable<Transactions> Post()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
--- SiGG-Payment-Gateway/SiGG-Payment-Gateway/Payment-Gateway.cs
+++ SiGG-Payment-Gateway/SiGG-Payment-Gateway/Payment-Gateway.cs
@@ -18,4 +18,16 @@ namespace SiGG_Payment_Gateway
         public string? Summary { get; set; }
     }
 
+    public class Transactions
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
 }
+
+
